9d73fdefb707c48beb4b2e1e91db25dbc9a5da1c
Maxv1z <meitstudying@gmail.com>
2023-06-12 17:38:52 +0300

    Functional components version

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -2,21 +2,19 @@ import { Component } from "react";
 
 import "./card-container.styles.css";
 
-class CardContainer extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
+const CardContainer = (props) => {
+  const { id, name, email } = props.monster;
 
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default CardContainer;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,18 +3,16 @@ import { Component } from "react";
 import "./card-list.styles.css";
 import CardContainer from "../card-container/card-container.component";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
+const CardList = (props) => {
+  const {monsters} = props;
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <CardContainer monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <CardContainer monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
